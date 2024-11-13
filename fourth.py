1f26c9b1996a696416500b5ca5d8d940fbdcee5b
johnhasflight <olivercrossleyfordham.30@gmail.com>
2024-11-13 14:36:39 +0000

    fourth.py

---
+++ fourth.py
@@ -0,0 +1,11 @@
+import turtle
+Dave = turtle.Turtle()
+Dave.shape("turtle")
+Dave.color = ("Red")
+
+for x in range(24):
+  Dave.speed(15)
+  Dave.forward(45)
+  Dave.left(85)
+  Dave.left(85)
+  Dave.forward(80)
